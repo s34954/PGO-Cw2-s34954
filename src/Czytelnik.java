cd0f15c0b94732851c2fd3a1788b224f9e20e2b0
Oskar Wieczorek <planacyblack@gmail.com>
2026-04-01 18:45:22 +0200

    Dodanie klasy Czytelnik

--- src/Czytelnik.java
+++ src/Czytelnik.java
@@ -1,31 +1,29 @@
-public class Czytelnik {
-    private String imie;
-    private String nazwisko;
-    private int numerKarty;
-    private int liczbaWypozyczen;
 
-    public Czytelnik(String imie, String nazwisko, int numerKarty) {
-        this.imie = imie;
-        this.nazwisko = nazwisko;
-        this.numerKarty = numerKarty;
-        this.liczbaWypozyczen = 0;
-    }
+    public class Czytelnik {
+        private String imie;
+        private String nazwisko;
+        private int numerKarty;
+        private int liczbaWypozyczen;
 
-    public void wypiszDane() {
-        System.out.println(imie + " " + nazwisko);
-        System.out.println("Nr karty: " + numerKarty);
-        System.out.println("Wypozyczenia: " + liczbaWypozyczen);
-        System.out.println("----------------------");
-    }
+        public Czytelnik(String imie, String nazwisko, int numerKarty) {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+            this.numerKarty = numerKarty;
+            this.liczbaWypozyczen = 0;
+        }
 
-    public void zwiekszLiczbeWypozyczen() {
-        liczbaWypozyczen++;
-    }
+        public void wypiszDane() {
+            System.out.println(imie + " " + nazwisko);
+            System.out.println("Nr karty: " + numerKarty);
+            System.out.println("Wypozyczenia: " + liczbaWypozyczen);
+        }
 
-    public void zmniejszLiczbeWypozyczen() {
-        if (liczbaWypozyczen > 0) {
-            liczbaWypozyczen--;
+        public void zwiekszLiczbeWypozyczen() {
+            liczbaWypozyczen++;
+        }
+
+        public void zmniejszLiczbeWypozyczen() {
+            if (liczbaWypozyczen > 0) liczbaWypozyczen--;
         }
     }
-}
 
